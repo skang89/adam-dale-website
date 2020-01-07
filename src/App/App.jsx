3287b4653144e4dfd51f795b2c55b77c7020c764
Shiv Kang <shivkang89@gmail.com>
2020-01-07 11:14:27 +0000

    removed default cra files and added basic index and app files

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+// import react packages
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adam Dale Website</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+// import react packages
+import React from "react";
+import ReactDOM from "react-dom";
+
+// import serviceworker
+import * as serviceWorker from "./serviceWorker";
+
+// import react component
+import App from "./App/App";
+
+ReactDOM.render(<App />, document.getElementById("adamWebsite"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
