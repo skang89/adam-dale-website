0ecb2f61757cd3072e8fd1504c00a26ac43fe181
Shiv Kang <shivkang89@gmail.com>
2020-01-07 13:41:00 +0000

    updated favicon and manifest file, added bulma to project

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,15 @@
 // import react packages
 import React from "react";
 
+// import styles
+import "./App.scss";
+import Navbar from "../Components/Navbar/Navbar";
+
 const App = () => {
   return (
     <div>
       <h1>Adam Dale Website</h1>
+      <Navbar />
     </div>
   );
 };
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,8 @@
+// import react packages
+import React from "react";
+
+const Navbar = () => {
+  return <h2>navbar</h2>;
+};
+
+export default Navbar;
