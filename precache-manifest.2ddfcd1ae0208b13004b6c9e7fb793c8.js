26149519b48583dda071a4d765311be7a0584e17
Shiv Kang <shivkang89@gmail.com>
2020-01-07 20:50:33 +0000

    Updates

---
+++ precache-manifest.2ddfcd1ae0208b13004b6c9e7fb793c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f61a515bb20073ccd9a88f55e3b4a7b",
+    "url": "/adam-dale-website/index.html"
+  },
+  {
+    "revision": "8dd48404c9eecfb9e569",
+    "url": "/adam-dale-website/static/css/main.6460732f.chunk.css"
+  },
+  {
+    "revision": "384594a7c52447911f94",
+    "url": "/adam-dale-website/static/js/2.e1afaae2.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/adam-dale-website/static/js/2.e1afaae2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8dd48404c9eecfb9e569",
+    "url": "/adam-dale-website/static/js/main.3daad20d.chunk.js"
+  },
+  {
+    "revision": "a6fe9b227270aae2dcd3",
+    "url": "/adam-dale-website/static/js/runtime-main.d23eb2c0.js"
+  }
+]);
