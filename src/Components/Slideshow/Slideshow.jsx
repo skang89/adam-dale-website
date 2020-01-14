9ecfbcdd23f2886cc3ab50bf760e08057e38ae3b
Shiv Kang <shivkang89@gmail.com>
2020-01-14 16:56:57 +0000

    amended routing and slideshow responsiveness

--- src/Components/Slideshow/Slideshow.jsx
+++ src/Components/Slideshow/Slideshow.jsx
@@ -22,92 +22,186 @@ const Slideshow = () => {
   };
 
   return (
-    <section className="adw-slider">
-      <Slider {...settings}>
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/WMCA_Brindley_Place.jpg"
-            alt="West Midlands Combined Authority Brindley Place"
-          />
-        </div>
+    <>
+      <section className="adw-slider is-hidden-mobile">
+        <Slider {...settings}>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/WMCA_Brindley_Place.jpg"
+              alt="West Midlands Combined Authority Brindley Place"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/WMCA_Library.jpg"
-            alt="West Midlands Combined Authority Library"
-          />
-        </div>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/WMCA_Library.jpg"
+              alt="West Midlands Combined Authority Library"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/HI_Logo.png" alt="HI logo" />
-          <img src="" alt="HI logo" className="is-hidden-tablet" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/HI_Logo.png" alt="HI logo" />
+            <img src="" alt="HI logo" className="is-hidden-tablet" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/HI_Beer_Can.jpg" alt="HI beer can" />
-        </div>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/HI_Beer_Can.jpg"
+              alt="HI beer can"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/HI_Mobile_Website.jpg"
-            alt="HI mobile website"
-          />
-        </div>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/HI_Mobile_Website.jpg"
+              alt="HI mobile website"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/She_Lead.png" alt="She lead" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/She_Lead.png" alt="She lead" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/Ittimaas.png" alt="Ittimaas" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/Ittimaas.png" alt="Ittimaas" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/Ittimaas_Stationary.jpg"
-            alt="Ittimaas stationary"
-          />
-        </div>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/Ittimaas_Stationary.jpg"
+              alt="Ittimaas stationary"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/SOB_Logos.png" alt="SOB Logo" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/SOB_Logos.png" alt="SOB Logo" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/SOB_Posters.jpg" alt="SOB poster" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/SOB_Posters.jpg" alt="SOB poster" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/SOB_Book_Cover.jpg"
-            alt="SOB book cover"
-          />
-        </div>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/SOB_Book_Cover.jpg"
+              alt="SOB book cover"
+            />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/Kachun.png" alt="Kachun" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/Kachun.png" alt="Kachun" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/CM_Font.png" alt="CM Font" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/CM_Font.png" alt="CM Font" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/CM_Wall.jpg" alt="CM  Wall" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/CM_Wall.jpg" alt="CM  Wall" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img src="assets/images/desktop/NCVO.png" alt="NCVO" />
-        </div>
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/NCVO.png" alt="NCVO" />
+          </div>
 
-        <div className="adw-slider__item is-hidden-mobile">
-          <img
-            src="assets/images/desktop/David_Cobley.jpg"
-            alt="David Cobley"
-          />
-        </div>
-      </Slider>
-    </section>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/David_Cobley.jpg"
+              alt="David Cobley"
+            />
+          </div>
+        </Slider>
+      </section>
+      <section className="adw-slider is-hidden-tablet">
+        <Slider {...settings}>
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/WMCA_Brindley_Place.jpg"
+              alt="West Midlands Combined Authority Brindley Place"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/WMCA_Library.jpg"
+              alt="West Midlands Combined Authority Library"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/HI_Logo.png" alt="HI logo" />
+            <img src="" alt="HI logo" className="is-hidden-tablet" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/HI_Beer_Can.jpg"
+              alt="HI beer can"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/HI_Mobile_Website.jpg"
+              alt="HI mobile website"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/She_Lead.png" alt="She lead" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/Ittimaas.png" alt="Ittimaas" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/Ittimaas_Stationary.jpg"
+              alt="Ittimaas stationary"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/SOB_Logos.png" alt="SOB Logo" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/SOB_Posters.jpg" alt="SOB poster" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/SOB_Book_Cover.jpg"
+              alt="SOB book cover"
+            />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/Kachun.png" alt="Kachun" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/CM_Font.png" alt="CM Font" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/CM_Wall.jpg" alt="CM  Wall" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img src="assets/images/desktop/NCVO.png" alt="NCVO" />
+          </div>
+
+          <div className="adw-slider__item">
+            <img
+              src="assets/images/desktop/David_Cobley.jpg"
+              alt="David Cobley"
+            />
+          </div>
+        </Slider>
+      </section>
+    </>
   );
 };
 
